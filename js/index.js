e535f0a3f68012e28e2a1151b6617fbe5c183f48
Marcus Costa Braga <volfgam@gmail.com>
2018-03-18 16:22:04 -0300

    Detalhes do book funcionando

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 var index = new Vue({
     el: '#content',
     data: {
-        bookName: "Lord of the rings",
-        books: null
+        bookName: "Javascript",
+        books: null,
+        title: null,
+        subtitle: null,
+        authors: null,
+        publisher: null,
+        publishedDate: null,
+        description: null
     },
     methods: {
         search: function () {
@@ -23,6 +29,18 @@ var index = new Vue({
                 dataType: "json",
                 async: true
             });
+        },
+        bookDetails: function(index) {
+            var self = this;
+            console.log(index);
+            self.title = self.books[index].volumeInfo.title;
+            self.subtitle = self.books[index].volumeInfo.subtitle;
+            self.authors = self.books[index].volumeInfo.authors;
+            self.publisher = self.books[index].volumeInfo.publisher;
+            self.publishedDate = self.books[index].volumeInfo.publishedDate;
+            self.description = self.books[index].volumeInfo.description;
+            
+            $('#bookDetails').modal('show');
         }
     },
     created: function () {
